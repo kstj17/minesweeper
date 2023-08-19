152b8b1667066c0fd3b4aee02ff2ebf6197e77a3
s_kseniya <xenast98@gmail.com>
2023-08-20 00:24:56 +0300

    class Game 1.32

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,11 +3,13 @@ import java.awt.*;
 import java.util.Locale;
 import sweeper.Box;
 import sweeper.Coord;
+import sweeper.Game;
 import sweeper.Ranges;
 
 public class JavaSweeper extends JFrame {
 
     private JPanel panel;
+    private Game game;
     private final int COLS = 9;
     private final int ROWS = 9;
     private final int IMAGE_SIZE = 50;
@@ -25,17 +27,16 @@ public class JavaSweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for (Coord coord : Ranges.getAllCoords()) {
-                    g.drawImage((Image) Box.values()[(coord.x + coord.y) % Box.values().length].image,
+                for (Coord coord : Ranges.getAllCoords())
+                    g.drawImage((Image) game.getBox(coord).image,
                             coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
-                }
             }
         };
         panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE, Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
     private JavaSweeper(){
-        Ranges.setSize(new Coord(COLS, ROWS));
+        game = new Game(COLS, ROWS);
         setImages();
         initPanel();
         initFrame();
