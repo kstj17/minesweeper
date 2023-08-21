d1679f6424f398ff0a4e318882f35a857a57780a
s_kseniya <xenast98@gmail.com>
2023-08-21 17:55:49 +0300

    2.30

--- src/sweeper/Bomb.java
+++ src/sweeper/Bomb.java
@@ -1,34 +1,45 @@
 package sweeper;
 
- class Bomb
-{
- private Matrix bombMap;
- private int totalBombs;
+class Bomb {
+     private Matrix bombMap;
+     private int totalBombs;
 
- Bomb(int totalBombs)
- {
-     this.totalBombs = totalBombs;
- }
- public void start()
- {
-    bombMap = new Matrix(Box.ZERO);
-    for (int i = 0; i < totalBombs; i++)
-    placeBomb();
- }
- Box get (Coord coord)
- {
-  return bombMap.get(coord);
- }
- private void placeBomb()
- {
-     Coord coord = Ranges.getRandomCoord();
-     bombMap.set(coord, Box.BOMB);
-     incNumberAroundBomb(coord);
- }
+     Bomb(int totalBombs)
+     {
+         this.totalBombs = totalBombs;
+         fixBombCount();
+     }
+     public void start()
+     {
+        bombMap = new Matrix(Box.ZERO);
+        for (int i = 0; i < totalBombs; i++)
+        placeBomb();
+     }
+     Box get (Coord coord)
+     {
+      return bombMap.get(coord);
+     }
 
- private void incNumberAroundBomb(Coord coord){
-     for (Coord around : Ranges.getCoordsAround(coord))
-         if(Box.BOMB != bombMap.get(around))
-             bombMap.set(around, bombMap.get(around).getNextNumberBox());
- }
+     private void fixBombCount(){
+         int maxBombs = Ranges.getSize().x * Ranges.getSize().y / 2;
+         if(totalBombs > maxBombs) totalBombs = maxBombs;
+     }
+     private void placeBomb()
+     {
+         while (true)
+         {
+         Coord coord = Ranges.getRandomCoord();
+         if (Box.BOMB == bombMap.get(coord))
+             continue;
+             bombMap.set(coord, Box.BOMB);
+             incNumberAroundBomb(coord);
+             break;
+         }
+     }
+
+     private void incNumberAroundBomb(Coord coord){
+         for (Coord around : Ranges.getCoordsAround(coord))
+             if(Box.BOMB != bombMap.get(around))
+                 bombMap.set(around, bombMap.get(around).getNextNumberBox());
+     }
 }
